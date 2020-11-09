c9df33223f7c224c234ce414adc5b6612bceada2
Anirudh H M <anihm136@gmail.com>
2020-11-09 18:39:50 +0530

    Make command async
    The `:UpdateImports` command now emits an error if the index is not yet
    built. Index builds in the background as usual, and notifies the user
    when it is ready

--- rplugin/python3/importmagic_vim.py
+++ rplugin/python3/importmagic_vim.py
@@ -1,27 +1,60 @@
+import threading
+
 import importmagic
 import pynvim
+
+g_nvim = None
+g_index = None
+g_index_built = False
+g_index_path = None
+
+
+def as_thread(target, *a, **b):
+    def _target():
+        try:
+            t.result = target()
+        except Exception as exc:
+            t.failure = exc
+
+    t = threading.Thread(target=_target, *a, **b)
+    return t
 
 
 @pynvim.plugin
 class ImportMagicVim:
     def __init__(self, nvim):
-        self.nvim = nvim
-        self.index = importmagic.SymbolIndex()
-        self.index_built = False
-        self.update_source()
+        global g_nvim
+        global g_index
+        global g_index_built
+        g_nvim = nvim
+        g_index = importmagic.SymbolIndex()
+        self.updateSource()
+
+    def _createindex(self):
+        global g_index_built
+        g_nvim.async_call(g_nvim.out_write, "Building importmagic index...\n")
+        g_index.build_index(g_index_path)
+        g_index_built = True
+        g_nvim.async_call(g_nvim.out_write, "Importmagic index built\n")
 
     @pynvim.function("CreateIndex")
-    def create_index(self, _):
-        a = self.nvim.command_output(
+    def createIndex(self, _):
+        global g_index_path
+        a = g_nvim.command_output(
             "echo system('python -c \"import sys; print(sys.path)\"')"
         )
-        # self.nvim.out_write(a + "\n")
-        self.index.build_index(eval(a))
-        self.index_built = True
+        g_index_path = eval(a)
+        thread = as_thread(self._createindex)
+        thread.start()
+        # thread.join()
+        # g_nvim.out_write(str(getattr(thread, "failure", None)) + "\n")
+        # if getattr(thread, "failure", None):
+        #     g_nvim.out_write("Failed: {}...\n".format(thread.failure))
+        #     raise Exception(thread.failure)
 
-    def update_source(self, update=None):
+    def updateSource(self, update=None):
         if not update:
-            self.source = "\n".join(self.nvim.current.buffer[:])
+            self.source = "\n".join(g_nvim.current.buffer[:])
         else:
             start, end, u = update
             tmp = self.source.splitlines()
@@ -29,25 +62,25 @@ class ImportMagicVim:
             self.source = "\n".join(tmp)
 
     def get_update(self):
-        self.update_source()
+        self.updateSource()
         scope = importmagic.Scope.from_source(self.source)
         # self.nvim.out_write(repr(scope) + '\n')
         unresolved, unreferenced = scope.find_unresolved_and_unreferenced_symbols()
         if unresolved or unreferenced:
             start_line, end_line, import_block = importmagic.get_update(
-                self.source, self.index, unresolved, unreferenced
+                self.source, g_index, unresolved, unreferenced
             )
             import_block = import_block.splitlines()[:-1]
             return start_line, end_line, import_block
         else:
             return None
 
-    @pynvim.command("UpdateImports", nargs=0, sync=True)
-    def testcommand(self):
-        if not self.index_built:
-            self.nvim.err_write("Index is not built yet\n")
+    @pynvim.command("UpdateImports", nargs=0)
+    def updateImports(self):
+        if not g_index_built:
+            g_nvim.err_write("Index is not built yet\n")
         else:
             update = self.get_update()
             if update:
                 start_line, end_line, import_block = update
-                self.nvim.current.buffer[start_line:end_line] = import_block
+                g_nvim.current.buffer[start_line:end_line] = import_block
